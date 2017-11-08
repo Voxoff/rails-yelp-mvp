26e85cf2c3c3a11b0b62412ea71b697ecac6523e
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-08 12:55:20 +0000

    restaurant crud done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'restaurants', to: 'restaurant#index'
+
+  ### new above id otherswise he'll get confuszed
+  # get 'restaurants/new', to: 'restaurant#new'
+  # get 'restaurants/:id', to: 'restaurant#show'
+
+  # post 'restaurants', to: 'restaurants#creationte'
+  resources :restaurants do
+    resources :reviews
+  end
+  root to: 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Restaurant.create(name: Faker::Name.name, address: Faker::Address.street_name, category: %w( italian chinese japanese french belgian).sample, phone_number: Faker::PhoneNumber.phone_number)
+end
